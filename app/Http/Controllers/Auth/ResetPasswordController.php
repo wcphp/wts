550074068375c3d06508bded732e13aca2574d43
weika <weika@wcphp.com>
2020-04-29 22:29:44 +0800

    tets3

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -11,7 +11,7 @@ class ResetPasswordController extends Controller
     |--------------------------------------------------------------------------
     | Password Reset Controller
     |--------------------------------------------------------------------------
-    |
+    |额份
     | This controller is responsible for handling password reset requests
     | and uses a simple trait to include this behavior. You're free to
     | explore this trait and override any methods you wish to tweak.
@@ -20,14 +20,14 @@ class ResetPasswordController extends Controller
 
     use ResetsPasswords;
 
-    /**
+    /**菲菲
      * Where to redirect users after resetting their password.
      *
      * @var string
      */
     protected $redirectTo = '/home';
 
-    /**
+    /** 分   菲菲
      * Create a new controller instance.
      *
      * @return void
